6684e324eb2d4663df1ae765fd6340aa1e377c38
jurcakmatej <jurcak.matej@gmail.com>
2016-09-20 17:42:58 +0200

    youtubedl folders

--- youtubelinks.py
+++ youtubelinks.py
@@ -1,6 +1,6 @@
 from bs4 import BeautifulSoup
 import requests
-from download import dowload
+from download import download
 
 def youtubelinks(title, path):
     youtubeurl = 'https://www.youtube.com/results?search_query=' + title
@@ -8,4 +8,4 @@ def youtubelinks(title, path):
     soup = BeautifulSoup(site.text, "lxml")
     first_video = soup.select('ol.section-list ol.item-section a:nth-of-type(1)')
     newurl = 'https://www.youtube.com' + first_video[0]['href']
-    dowload(newurl, path)
+    download(newurl, path)
